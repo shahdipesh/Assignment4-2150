b886e6a65d6369dd426ab108e829832c6a67b556
dipeshasd <dipesh.sah@hotmail.com>
2022-04-25 17:17:41 -0500

    removing comma from output

---
+++ Main.js
@@ -0,0 +1,22 @@
+//-----------------------------------------
+// NAME		: Dipesh Shah
+// STUDENT NUMBER	: 7882947
+// COURSE		: COMP 2150
+// INSTRUCTOR	: Ali Neshati
+// ASSIGNMENT	: assignment 4
+// QUESTION	: question 1
+//
+// REMARKS: Generate huffman encoding
+//
+//
+//-----------------------------------------
+
+let Encoder = require('./Encoder');
+
+class Main{
+    constructor() {
+    }
+    
+}
+let encoder = new Encoder("./Input.txt");
+encoder.encode();
